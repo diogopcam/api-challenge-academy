491031b0ddbbcec445be2368b0de61bc4182b7e5
Eduardo Ferrari <eduardoferrari@10-41-50-176.0200.epoa.br.iosda.org>
2025-08-13 16:44:40 -0300

    enum Category

---
+++ api-challenge/Model/Category.swift
@@ -0,0 +1,33 @@
+//
+//  Category.swift
+//  api-challenge
+//
+//  Created by Eduardo Ferrari on 13/08/25.
+//
+
+enum Category: String, CaseIterable, Codable {
+    case beauty = "beauty"
+    case fragrances = "fragrances"
+    case furniture = "furniture"
+    case groceries = "groceries"
+    case homeDecoration = "home-decoration"
+    case kitchenAccessories = "kitchen-accessories"
+    case laptops = "laptops"
+    case mensShirts = "mens-shirts"
+    case mensShoes = "mens-shoes"
+    case mensWatches = "mens-watches"
+    case mobileAccessories = "mobile-accessories"
+    case motorcycle = "motorcycle"
+    case skinCare = "skin-care"
+    case smartphones = "smartphones"
+    case sportsAccessories = "sports-accessories"
+    case sunglasses = "sunglasses"
+    case tablets = "tablets"
+    case tops = "tops"
+    case vehicle = "vehicle"
+    case womensBags = "womens-bags"
+    case womensDresses = "womens-dresses"
+    case womensJewellery = "womens-jewellery"
+    case womensShoes = "womens-shoes"
+    case womensWatches = "womens-watches"
+}
