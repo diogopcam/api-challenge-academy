b824278201add56ee0a59fd92ae5c02bee291ce5
Diogo <134295616+diogopcam@users.noreply.github.com>
2025-08-13 15:53:26 -0300

    Add files via upload

---
+++ api-challenge-academy/api_challenge_academyApp.swift
@@ -0,0 +1,32 @@
+//
+//  api_challenge_academyApp.swift
+//  api-challenge-academy
+//
+//  Created by Diogo Camargo on 13/08/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct api_challenge_academyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
