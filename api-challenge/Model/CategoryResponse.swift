9830d150ab1f4992e07d88501f4c05dbf8265389
Diogo Pessin Ca <diogpcam@10-41-56-1.0200.epoa.br.iosda.org>
2025-08-13 16:37:43 -0300

    Resposta de category.

---
+++ api-challenge/Model/CategoryResponse.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryResponse.swift
+//  api-challenge
+//
+//  Created by Diogo Camargo on 13/08/25.
+//
+
+import Foundation
+
+struct CategoryResponse: Decodable {
+    let categories: [String]
+}
